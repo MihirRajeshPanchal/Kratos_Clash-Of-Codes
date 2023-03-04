16623dd0f3feafe23b4775a36933aa57b2dde0d8
Chintan Dodia <chintan222005@gmail.com>
2023-03-04 13:10:40 +0530

    .

--- Home.py
+++ Home.py
@@ -9,8 +9,9 @@ st.set_page_config(
     page_title="Sizzle - Home",
 )
 
-
-st.button("Mihir")
+logo = "Images\Screenshot 2023-03-04 125955-2.png"
+st.image(logo, use_column_width=True)
+#st.button("Mihir")
 
 hide_streamlit_style = """
             <style>
