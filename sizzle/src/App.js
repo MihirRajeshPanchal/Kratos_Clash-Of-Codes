f8b4adfa29fa1a215a7b579a52280f8c1e0f5746
Sarid10 <saridqureshi299@gmail.com>
2023-03-04 16:50:25 +0530

    added login page

--- sizzle/src/App.js
+++ sizzle/src/App.js
@@ -14,6 +14,7 @@ import Navbar from './components/navbar'
 import Footer from './components/footer'
 import Body from './components/body'
 import Signup from './pages/Signup'
+import Login from './pages/Login'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
@@ -25,6 +26,7 @@ function App() {
         <Routes>
           <Route path="*" element={ <Body /> } />
           <Route path="/signup" element={ <Signup /> } />
+          <Route path="/login" element={ <Login /> } />
         </Routes>
       </Router>
       <Footer />
--- sizzle/src/components/navbar.js
+++ sizzle/src/components/navbar.js
@@ -83,7 +83,7 @@ import {
               fontSize={'sm'}
               fontWeight={400}
               variant={'link'}
-              href={'#'}>
+              href={'/login'}>
               Log In
             </Button>
             
@@ -104,6 +104,7 @@ import {
             <Menu>
               <MenuButton>
                 <Avatar
+                  
                   size={'sm'}
                   src={
                     'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
@@ -273,12 +274,16 @@ import {
   const NAV_ITEMS: Array<NavItem> = [
     {
       label: 'Home',
-      href: ''
+      href: '/'
       
     },
     {
       label: 'Discover',
       href: ''
+    },
+    {
+      label: 'My Profile',
+      href: '/profile'
     },
     {
       label: 'My Likes',
